2d8c60ff5e0382e7bfa3ace8f385f3728e931c4a
Rohde Fischer <rohdef@rohdef.dk>
2017-01-03 16:27:58 +0100

    Trying to gather more details about dbus

--- startPulse.sh
+++ startPulse.sh
@@ -1,21 +1,11 @@
 #!/bin/env bash
 
-echo Downloading dbus...
-wget http://dbus.freedesktop.org/releases/dbus/dbus-1.6.30.tar.gz -O dbus.tar.gz -q
-echo Unpacking dbus...
-tar -zxvf dbus.tar.gz > /dev/null
-rm dbus.tar.gz
-
-cd dbus-1.6.30
-
-./configure
-make
-sudo make install
-
-cd ..
+systemctl status dbus.socket dbus.service
 
 dbus-launch --version
 dbus-launch
+
+systemctl status dbus.socket dbus.service
 
 pulseaudio -v
 pulseaudio --start
