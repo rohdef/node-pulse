56da512ac6011ae056f39eb418527853b18b7cf1
Rohde Fischer <rohdef@rohdef.dk>
2017-01-03 16:35:14 +0100

    Trying to get further information

--- startPulse.sh
+++ startPulse.sh
@@ -1,11 +1,31 @@
 #!/bin/env bash
 
-systemctl status dbus.socket dbus.service
+echo "===="
+ls /var/run/
 
+echo "===="
+ls /var/run/dbus
+
+echo "===="
 dbus-launch --version
 dbus-launch
 
-systemctl status dbus.socket dbus.service
+echo "===="
+ls /var/run/
 
+echo "===="
+ls /var/run/dbus
+
+mkdir -p /var/run/dbus
+ln -s /tmp/dbus* /var/run/dbus/system_bus_socket
+
+
+echo "===="
+ls /var/run/
+
+echo "===="
+ls /var/run/dbus
+
+echo "===="
 pulseaudio -v
 pulseaudio --start
