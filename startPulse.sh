33a2be3a744ae39ebe37d7a6003ad5dfcbfb563c
Rohde Fischer <rohdef@rohdef.dk>
2017-01-03 16:24:03 +0100

    Trying with compiled dbus just in case

--- startPulse.sh
+++ startPulse.sh
@@ -1,5 +1,19 @@
 #!/bin/env bash
 
+echo Downloading dbus...
+wget http://dbus.freedesktop.org/releases/dbus/dbus-1.6.30.tar.gz -O dbus.tar.gz -q
+echo Unpacking dbus...
+tar -zxvf dbus.tar.gz > /dev/null
+rm dbus.tar.gz
+
+cd dbus-1.6.30
+
+./configure
+make
+sudo make install
+
+cd ..
+
 dbus-launch --version
 dbus-launch
 
